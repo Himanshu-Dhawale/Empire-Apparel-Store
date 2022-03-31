fed3ed210b44e858ca45701c689ee23115c9dba8
Himanshu Dhawale <80173512+Himanshu-Dhawale@users.noreply.github.com>
2022-03-31 13:14:26 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { makeServer } from "./server";
 import {ProductProvider} from "./context/productcontext"
-// Call make Server
 makeServer();
 
 ReactDOM.render(
